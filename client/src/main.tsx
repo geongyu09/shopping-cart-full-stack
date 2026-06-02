53ceae4950026fc1e693f273d7281c99eb085499
박건규 <geongyu09@gmail.com>
2026-06-02 20:32:57 +0900

    chore: Noto Sans 폰트 적용
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "@styles/globalStyle.css";
 
 async function enableMocking() {
   if (!import.meta.env.DEV) {
-    return
+    return;
   }
 
-  const { worker } = await import('./mocks/browser.ts')
-  return worker.start()
+  const { worker } = await import("./mocks/browser.ts");
+  return worker.start();
 }
 
 enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
     </StrictMode>,
-  )
-})
+  );
+});
