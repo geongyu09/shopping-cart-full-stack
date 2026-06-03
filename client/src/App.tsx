7c7bb09f2864043627e6a0dc96e94511a2a0f355
박건규 <geongyu09@gmail.com>
2026-06-03 14:47:33 +0900

    feat: '/'로 진입시 '/carts'로 리다이렉트 하도록 라우팅 설정
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 import CartsPage from "@pages/CartsPage";
 import OrderConfirmPage from "@pages/OrderConfirmPage";
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to={ROUTES.CARTS} replace />} />
         <Route path={ROUTES.CARTS} element={<CartsPage />} />
         <Route path={ROUTES.ORDER_CONFIRM} element={<OrderConfirmPage />} />
       </Routes>
