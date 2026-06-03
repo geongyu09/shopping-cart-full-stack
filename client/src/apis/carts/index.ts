cc17325f54078868b6496902683f217e92b9393b
박건규 <geongyu09@gmail.com>
2026-06-03 17:23:26 +0900

    feat: getCart api 로직 응답 타입 추가
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/apis/carts/index.ts
+++ client/src/apis/carts/index.ts
@@ -2,6 +2,25 @@ import fetcher from "../instance";
 
 const CARTS_API = "/carts";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface Cart {
+  product: Product;
+  quantity: number;
+}
+
+interface GetCartResponse {
+  status: "success" | "error";
+  message: string;
+  data: Cart[];
+}
+
 export const getCart = async () => {
-  return fetcher.get(`${CARTS_API}`);
+  const { data } = await fetcher.get<GetCartResponse>(`${CARTS_API}`);
+  return data;
 };
