722d7cf6485979b2ff02d6dce81116313602e025
박건규 <geongyu09@gmail.com>
2026-06-03 14:39:04 +0900

    chore: pages path alias 적용
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
       '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
     },
