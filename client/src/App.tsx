a9188d6719ff74becafc1c5322c00727a42ba377
박건규 <geongyu09@gmail.com>
2026-06-03 14:45:00 +0900

    refactor: route 상수화
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import CartsPage from "@pages/CartsPage";
 import OrderConfirmPage from "@pages/OrderConfirmPage";
+import { ROUTES } from "@constants/routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/carts" element={<CartsPage />} />
-        <Route path="/order-confirm" element={<OrderConfirmPage />} />
+        <Route path={ROUTES.CARTS} element={<CartsPage />} />
+        <Route path={ROUTES.ORDER_CONFIRM} element={<OrderConfirmPage />} />
       </Routes>
     </BrowserRouter>
   );
