a267cd14724e25a1f416759ecd0b075747e3f9d9
박건규 <geongyu09@gmail.com>
2026-06-04 17:51:03 +0900

    feat: OrderConfirmContent 컴포넌트 구현
    Co-authored-by: 송아영 <azerosong@gmail.com>

---
+++ client/src/components/common/OrderConfirmContent/index.tsx
@@ -0,0 +1,29 @@
+import Spacing from "@components/common/Spacing";
+import styled from "@emotion/styled";
+
+export default function OrderConfirmContent() {
+  return (
+    <>
+      <OrderConfirmHeading>주문 확인</OrderConfirmHeading>
+      <Spacing size={1.5} />
+      <OrderConfirmDescription>
+        총 2종류의 상품 4개를 주문합니다.
+      </OrderConfirmDescription>
+      <OrderConfirmDescription>
+        최종 결제 금액을 확인해 주세요.
+      </OrderConfirmDescription>
+    </>
+  );
+}
+
+const OrderConfirmHeading = styled.h2`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 100%;
+`;
+
+const OrderConfirmDescription = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 150%;
+`;
