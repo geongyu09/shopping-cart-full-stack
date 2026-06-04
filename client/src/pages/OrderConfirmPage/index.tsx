06edd52c9ada5ca70d26cb3c0c0ec74aaa10f603
박건규 <geongyu09@gmail.com>
2026-06-04 17:51:25 +0900

    feat: OrderConfirmPage 컴포넌트 구현
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/pages/OrderConfirmPage/index.tsx
+++ client/src/pages/OrderConfirmPage/index.tsx
@@ -1,3 +1,12 @@
+import GoBackButton from "@components/common/GoBackButton";
+import Header from "@components/common/Header";
+import OrderConfirmSection from "@components/feature/OrderConfirmSection";
+
 export default function OrderConfirmPage() {
-  return <></>;
+  return (
+    <>
+      <Header LeftComponent={<GoBackButton />} />
+      <OrderConfirmSection />
+    </>
+  );
 }
