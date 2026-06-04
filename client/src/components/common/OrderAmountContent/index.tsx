0fffbde8f47c708a328e5068766d00cc202c187a
박건규 <geongyu09@gmail.com>
2026-06-04 17:50:57 +0900

    feat: OrderAmountContent 컴포넌트 구현
    Co-authored-by: 송아영 <azerosong@gmail.com>

---
+++ client/src/components/common/OrderAmountContent/index.tsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+import Spacing from "../Spacing";
+
+export default function OrderAmountContent() {
+  return (
+    <>
+      <OrderAmountHeading>총 결제 금액</OrderAmountHeading>
+      <Spacing size={0.75} />
+      <OrderAmountDescription>120,000원</OrderAmountDescription>
+    </>
+  );
+}
+
+const OrderAmountHeading = styled.h3`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 16px;
+`;
+
+const OrderAmountDescription = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 100%;
+`;
