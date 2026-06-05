904463d17cf348cebe825341592b9d034964710d
박건규 <geongyu09@gmail.com>
2026-06-05 15:03:20 +0900

    feat: Header에 sticky 속성 추가
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/components/common/shared/Header/index.tsx
+++ client/src/components/common/shared/Header/index.tsx
@@ -24,6 +24,9 @@ const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 `;
 
 const ComponentContainer = styled.div``;
