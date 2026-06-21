a3034ebe7191716a400219232a2086a42b4d047e
박건규 <geongyu09@gmail.com>
2026-06-21 16:12:23 +0900

    feat(client): CartListSection 컴포넌트 구현

---
+++ client/src/components/feature/CartListSection/index.tsx
@@ -0,0 +1,185 @@
+import type { Product } from "@/types/cartProduct";
+import CartItem from "@components/common/entities/CartItem";
+import CartOrderAmount from "@components/common/entities/CartOrderAmount";
+import CheckBox from "@components/common/shared/CheckBox";
+import Divider from "@components/common/shared/Divider";
+import Spacing from "@components/common/shared/Spacing";
+import styled from "@emotion/styled";
+import useCartItemDeleteMutation from "@hooks/feature/useCartItemDeleteMutation";
+import useCartQuantityUpdateMutation from "@hooks/feature/useCartQuantityUpdateMutation";
+import useCartQuery from "@hooks/feature/useCartQuery";
+import useCheckedProductItems from "@hooks/feature/useCheckedProductItems";
+import { getCheckedItemsFromLocalStorage } from "@libs/carts/localstorage";
+import {
+  calcDeliveryFee,
+  calcOrderAmount,
+  calcTotalAmount,
+} from "@libs/carts/utils";
+import { COLOR_PALETTE } from "@styles/colorPalette";
+import { useEffect, useRef } from "react";
+
+function CartListSection() {
+  const { data: cartData } = useCartQuery();
+  const { mutate: quantityMutate } = useCartQuantityUpdateMutation();
+  const { mutate: deleteMutate } = useCartItemDeleteMutation();
+
+  const { checkedItems, select, unselect, unselectAll } =
+    useCheckedProductItems<Product["id"]>();
+
+  const isInitialized = useRef(false);
+
+  useEffect(
+    // 초기 렌더링 시 이전에 선택한 상품이 없다면 장바구니의 모든 상품을 선택하도록 설정
+    function initializeCheckedItems() {
+      if (isInitialized.current) return;
+      isInitialized.current = true;
+
+      const storedCheckedItems = getCheckedItemsFromLocalStorage().filter(
+        (id) => cartData.some(({ product }) => product.id === id),
+      );
+
+      if (storedCheckedItems.length === 0) {
+        cartData.forEach(({ product }) => select(product.id));
+      }
+    },
+    [cartData, select],
+  );
+
+  const isAllChecked = cartData.every(({ product }) =>
+    checkedItems.includes(product.id),
+  );
+
+  const isChecked = (id: number) => checkedItems.includes(id);
+
+  const handleSelectAll = () => {
+    if (isAllChecked) return unselectAll();
+
+    cartData.forEach(({ product }) => select(product.id));
+  };
+
+  const handleSelect = (id: number) => {
+    if (isChecked(id)) return unselect(id);
+
+    select(id);
+  };
+
+  const handleQuantityChange = (id: number, quantity: number) => {
+    quantityMutate(id, quantity);
+  };
+
+  const handleDelete = (id: number) => {
+    deleteMutate(id);
+    unselect(id);
+  };
+
+  if (cartData.length === 0)
+    return (
+      <EmptyCartContainer>
+        <EmptyCartText>장바구니에 담은 상품이 없습니다.</EmptyCartText>
+      </EmptyCartContainer>
+    );
+
+  return (
+    <>
+      <CartListContainer>
+        <SelectAllWrapper>
+          <CheckBox checked={isAllChecked} onChange={() => handleSelectAll()} />
+          전체선택
+        </SelectAllWrapper>
+        <Spacing size={1.25} />
+        <CartListWrapper>
+          {cartData.map(({ product, quantity }) => (
+            <CartItemContainer key={product.id}>
+              <Divider />
+              <Spacing size={0.75} />
+              <ActionButtonWrapper>
+                <CheckBox
+                  checked={checkedItems.includes(product.id)}
+                  onChange={() => handleSelect(product.id)}
+                />
+                <DeleteButton onClick={() => handleDelete(product.id)}>
+                  삭제
+                </DeleteButton>
+              </ActionButtonWrapper>
+              <Spacing size={0.75} />
+              <CartItem
+                {...product}
+                quantity={quantity}
+                quantityRange={{ min: 1, max: 99 }}
+                onChangeQuantity={(newQuantity) =>
+                  handleQuantityChange(product.id, newQuantity)
+                }
+              />
+            </CartItemContainer>
+          ))}
+        </CartListWrapper>
+        <Spacing size={3.25} />
+      </CartListContainer>
+      <CartOrderAmount
+        orderAmount={calcOrderAmount(cartData, checkedItems)}
+        deliveryFee={calcDeliveryFee(calcOrderAmount(cartData, checkedItems))}
+        totalAmount={calcTotalAmount(
+          calcOrderAmount(cartData, checkedItems),
+          calcDeliveryFee(calcOrderAmount(cartData, checkedItems)),
+        )}
+      />
+    </>
+  );
+}
+
+const EmptyCartContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const EmptyCartText = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 16px;
+`;
+
+const CartListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SelectAllWrapper = styled.label`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 0.9375rem;
+`;
+
+const CartListWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+`;
+
+const CartItemContainer = styled.li``;
+
+const ActionButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const DeleteButton = styled.button`
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  border: 1px solid ${COLOR_PALETTE.border};
+  background-color: ${COLOR_PALETTE.white};
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 0.9375rem;
+
+  :active {
+    background-color: ${COLOR_PALETTE.border};
+  }
+`;
+
+export default CartListSection;
