9c8794d110bd7c81c0e7df76e1610632628a39bd
박건규 <geongyu09@gmail.com>
2026-06-21 23:56:36 +0900

    feat(client): 모달 provider 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,19 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { ROUTES } from "@constants/routes";
 import CartsPage from "@pages/CartsPage";
 import OrderConfirmPage from "@pages/OrderConfirmPage";
+import { ModalProvider } from "@/service/modal";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to={ROUTES.CARTS} replace />} />
-        <Route path={ROUTES.CARTS} element={<CartsPage />} />
-        <Route path={ROUTES.ORDER_CONFIRM} element={<OrderConfirmPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ModalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={ROUTES.CARTS} replace />} />
+          <Route path={ROUTES.CARTS} element={<CartsPage />} />
+          <Route path={ROUTES.ORDER_CONFIRM} element={<OrderConfirmPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ModalProvider>
   );
 }
 
