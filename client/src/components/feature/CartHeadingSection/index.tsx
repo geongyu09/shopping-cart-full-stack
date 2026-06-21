c01c210f6278e131210c1106113f118cb6b7d50c
박건규 <geongyu09@gmail.com>
2026-06-21 16:02:15 +0900

    feat(client): CartHeadingSection 컴포넌트 구현

---
+++ client/src/components/feature/CartHeadingSection/index.tsx
@@ -0,0 +1,16 @@
+import SectionIntro from "@components/common/shared/SectionIntro";
+import useCartQuery from "@hooks/feature/useCartQuery";
+
+export default function CartHeadingSection() {
+  const { data: cartData } = useCartQuery();
+
+  const productCount = cartData.length;
+
+  const title = "장바구니";
+  const description =
+    cartData.length > 0
+      ? [`총 ${productCount}종류의 상품이 담겨있습니다.`]
+      : undefined;
+
+  return <SectionIntro title={title} description={description} />;
+}
