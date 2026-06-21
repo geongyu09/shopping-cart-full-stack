87086d041d72abfc6413174478007616054274eb
박건규 <geongyu09@gmail.com>
2026-06-21 15:57:07 +0900

    feat(client): useLocalStorageValue를 사용하여 장바구니에서 선택된 상품을 관리하는 useCheckedProductItems훅 구현

---
+++ client/src/hooks/feature/useCheckedProductItems.ts
@@ -0,0 +1,25 @@
+import useLocalStorageValue from "@hooks/common/useLocalStorageValue";
+
+const STORAGE_KEY = "checkedItems";
+
+export default function useCheckedProductItems<T>() {
+  const { value, setValue } = useLocalStorageValue<T[]>(STORAGE_KEY, []);
+
+  const select = (item: T) => {
+    setValue((prev) => [...new Set([...prev, item])]);
+  };
+
+  const unselect = (item: T) => {
+    setValue((prev) => prev.filter((prevItem) => !Object.is(prevItem, item)));
+  };
+
+  const unselectAll = () => {
+    setValue([]);
+  };
+
+  const selectAll = (items: T[]) => {
+    setValue(items);
+  };
+
+  return { checkedItems: value, select, unselect, unselectAll, selectAll };
+}
