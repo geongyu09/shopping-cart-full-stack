0d1fec5ecd30b1348fb6ca7d4b9153494634b68a
박건규 <geongyu09@gmail.com>
2026-06-21 15:52:04 +0900

    chore(client): libs alias 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       "@constants": fileURLToPath(new URL("./src/constants", import.meta.url)),
       "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
       "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      "@libs": fileURLToPath(new URL("./src/libs", import.meta.url)),
       "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
       "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
     },
