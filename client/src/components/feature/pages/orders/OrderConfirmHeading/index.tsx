a01da14ba0f1029d4557d153473f162e4107837c
박건규 <geongyu09@gmail.com>
2026-06-22 10:18:26 +0900

    fix(client): 주문 확인 헤딩 문구를 올바르게 수정

--- client/src/components/feature/pages/orders/OrderConfirmHeading/index.tsx
+++ client/src/components/feature/pages/orders/OrderConfirmHeading/index.tsx
@@ -14,7 +14,7 @@ function OrderConfirmHeading() {
 
   return (
     <SectionIntro
-      title="장바구니"
+      title="주문 확인"
       description={[
         `총 ${productCount}종류의 상품 ${totalQuantity}개를 주문합니다.`,
         `최종 결제 금액을 확인해 주세요.`,
